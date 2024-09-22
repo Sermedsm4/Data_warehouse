87e4cc274f5f26d1a9242ee2259613f0f785f693
Sermedsm4 <sermed-sm4@live.se>
2024-09-22 16:56:45 +0200

    Ta bort alla filer utom README

--- .dbt/grant_user.sql
+++
@@ -1,6 +0,0 @@
-USE ROLE ACCOUNTADMIN;
-CREATE ROLE job_ads_dbt_role;
-GRANT ROLE job_ads_dbt_role TO USER transformer;
-GRANT ROLE job_ads_dbt_role TO USER sermedsm4;
-
-
--- .dbt/setup_user.sql
+++
@@ -1,8 +0,0 @@
-USE ROLE USERADMIN;
-CREATE USER IF NOT EXISTS transformer
-   PASSWORD = "transformer_123"
-   DEFAULT_WAREHOUSE = dev_wh
-   LOGIN_NAME = "transformer"
-   DEFAULT_NAMESPACE="job_ads.warehouse"
-   COMMENT = "dbt user for transforming data"
-   DEFAULT_ROLE = "job_ads_dbt_role"; 
